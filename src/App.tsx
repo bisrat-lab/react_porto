5a6688af9fd4762247cfc856c483731cc5d68c6b
bisrat2020 <wow.bisrat11@gmail.com>
2023-06-12 10:52:18 -0400

    change

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,22 @@
 
 function App() {
   return (
+    <div>
+    
+    
     <div className="snap-y snap-mandatory h-screen overflow-scroll">
     <div className="snap-start bg-amber-200 w-screen h-screen flex items-center justify-center text-8xl">1</div>
     <div className="snap-start bg-teal-200 w-screen  h-screen flex items-center justify-center text-8xl">2</div>
     <div className="snap-start bg-cyan-200 w-screen h-screen flex items-center justify-center text-8xl">3</div>
     <div className="snap-start bg-fuchsia-200 w-screen h-screen flex items-center justify-center text-8xl">4</div>
+</div>
+ <a href="#hero">
+ <footer className="sticky bottom-12 w-full cursor-pointer">
+   <div className="flex items-center justify-center">
+    <p>B</p>
+   </div>
+ </footer>
+</a>
 </div>
   );
 }
